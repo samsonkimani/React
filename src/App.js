5e30acfbf3041c5076dec86cbf9ddc9f8a4d724f
samsonkimani <samsonkimani43@gmail.com>
2023-01-30 18:23:39 +0300

    creating a table in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-class App extends React.Component {
-    render() {
-      return (
-        <div className='App'>
-          <h1>Hello React</h1>
-        </div>
-      )
-    }
-  }
 
-  export default App
+import Table from './Table'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+      </div>
+    )
+  }
+}
+
+export default App
